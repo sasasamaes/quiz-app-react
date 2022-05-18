3ed553f3196c300eb3cec0323627d465beb37c84
Francisco Campos <hey@francampos.me>
2022-05-17 23:01:00 -0600

    init proyect

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+    </ThemeProvider>
     <App />
   </React.StrictMode>
 );
